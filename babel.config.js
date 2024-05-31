28ca577d5828ce4a91c4162261151b14f73e20c9
Anh Khong <anh.khong@bmbsoft.com.vn>
2024-05-31 14:07:04 +0700

    [Config] add package navigation and root import

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,21 @@
-module.exports = {
-  presets: ['module:@react-native/babel-preset'],
+module.exports = function (api) {
+  api.cache(true);
+  return {
+    presets: ['module:metro-react-native-babel-preset'],
+    plugins: [
+      [
+        'module-resolver',
+        {
+          root: ['./src'],
+        },
+      ],
+      [
+        'babel-plugin-root-import',
+        {
+          rootPathSuffix: 'src',
+        },
+      ],
+      ['react-native-reanimated/plugin'],
+    ],
+  };
 };
